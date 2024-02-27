3ad2e6eb79329933761aa054b17ebfcc13ec9afe
박겸 <dotori634@gmail.com>
2024-02-27 18:54:53 +0900

    [level 0] Title: 등수 매기기, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.js
@@ -0,0 +1,16 @@
+const solution = (score) => {
+  const answer = [];
+  let rank = 1;
+  for (let i = 0; i < score.length; i++) {
+    for (let j = 0; j < score.length; j++) {
+      if ((score[i][0] + score[i][1]) < (score[j][0] + score[j][1])) {
+        rank++;
+      } else if ((score[i][0] + score[i][1]) === (score[j][0] + score[j][1])) {
+        continue;
+      }
+    }
+    answer.push(rank);
+    rank = 1;
+  }
+  return answer;
+}
