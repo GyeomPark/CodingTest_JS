4b90f990f810eac74c411404bb404064f8990d21
박겸 <dotori634@gmail.com>
2024-04-17 01:16:39 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,9 +1,8 @@
 function solution(array, height) {
-    const arr = [];
-    for (let i = 0; i <= array.length; i++) {
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
         if (array[i] > height) {
-            arr.push(array[i]);
-        } 
+            return result.push();
+        }
     }
-    return arr.length;
 }
